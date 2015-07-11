8a239ac64786bad942609c4ce78742604762f057
jsl87 <jsl@pdx.edu>
2015-07-12 03:39:18 +0900

    fixed the state issue
    forgot to commit boot.ts file

---
+++ baby-trader/boot.ts
@@ -0,0 +1,27 @@
+﻿/****************************************************************************************
+Copyright (C) 2015 Jong Seong Lee
+jsl@pdx.edu
+Portland State University
+
+This program is licensed under the MIT License.
+Please see the file COPYING in the source distribution of this software for license terms.
+****************************************************************************************/
+
+module BabyTrader {
+    export class Boot extends Phaser.State {
+        constructor() {
+            super();
+        }
+
+        preload() {
+            this.game.load.image('preloadIcon', 'assets/graphics/preloader.gif');
+        }
+
+        create() {
+            this.game.state.start("preload");
+        }
+
+        update() {
+        }
+    }
+}
